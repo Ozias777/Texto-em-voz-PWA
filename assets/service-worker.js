ff28e821a653e1f5283b4cad5d8454970f2a329e
Ozias <oziassnascimento@gmail.com>
2024-04-11 16:33:54 -0300

    Update 4.0

--- assets/service-worker.js
+++ assets/service-worker.js
@@ -1,40 +1,30 @@
-var cacheName = 'pwaTeste-v1.0';
+var cacheName = 'pwaTeste+-v1.0';
 
-self.addEventListener('install', event => {
-    self.skipWaiting();
-    event.waitUntil(
-        caches.open(cacheName)
-            .then(cache => cache.addAll([
-                './index.html',
-                './assets/CSS/style.css',
-                './assets/JS/script.js',
-                './assets/CSS/img/logo-texto-em-voz.png',
-                './assets/CSS/img/logo-microphone.png',
-                // Caminho para as outras imagens que ficaram salvas no cache
-                './assets/img/29.png',
-                './assets/img/40.png',
-                './assets/img/57.png',
-                './assets/img/58.png',
-                './assets/img/60.png',
-                './assets/img/80.png',
-                './assets/img/180.png',
-                './assets/img/1024.png'
-            ]))
-    );
+self.addEventListener('install', function(event) {
+  event.waitUntil(
+    caches.open(cacheName).then(function(cache) {
+      return cache.addAll([
+        '/',
+        '/index.html',
+        '/assets/CSS/style.css',
+        '/assets/JS/script.js',
+        '/assets/img/29.png',
+        '/assets/img/40.png',
+        '/assets/img/57.png',
+        '/assets/img/58.png',
+        '/assets/img/60.png',
+        '/assets/img/80.png',
+        '/assets/img/180.png',
+        '/assets/img/1024.png',
+      ]);
+    })
+  );
 });
 
-self.addEventListener('message', function (event) {
-    if (event.data.action === 'skipWaiting') {
-        self.skipWaiting();
-    }
-});
-
-self.addEventListener('fetch', function (event) {
-    event.respondWith(async function () {
-        try {
-            return await fetch(event.request);
-        } catch (err) {
-            return caches.match(event.request);
-        }
-    }());
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request).then(function(response) {
+      return response || fetch(event.request);
+    })
+  );
 });
